e41a3694f641339419fcbfbd27f82ad09fce2074
RegianoAlves <regiano.alves@gmail.com>
2024-10-06 21:29:04 -0300

    Resolvendo conflitos na classe de testes.

--- src/Calculadora.test.js
+++ src/Calculadora.test.js
@@ -15,7 +15,7 @@ describe("Calculadora", () => {
     // expect(calculadora).toBeDefined(); 
     expect(calculadora.resultado).toBeDefined();
     expect(typeof calculadora.resultado).toBe('number');
-  });
+  })
 
   it('Atribui um novo valor à calculadora', () => {
     const calculadora = new Calculadora();
@@ -25,7 +25,7 @@ describe("Calculadora", () => {
     expect(calculadora.resultado).toBe(0);
     expect(() => calculadora.resultado = "5").not.toThrow(TypeError);
     expect(calculadora.resultado).toBe(5);
-  });
+  })
 
   it("Operação soma", () => {
     const calculadora = new Calculadora();
@@ -54,42 +54,6 @@ describe("Calculadora", () => {
     expect(calculadora.resultado).toBe(5);
 
     
-  });
-})
-
-
-
-
-
-
-
-
-
-
-
-// it("Criar nova calculadora", () => {
-//   const calculadora = new Calculadora();
-//   expect(calculadora).toBeDefined();
-//   expect(calculadora.resultado).toBeDefined();
-//   expect(typeof calculadora.resultado).toBe("number");
-//   expect(calculadora.resultado).toBe(0);
-// });
-
-// it("obtém resultado", () => {
-//   const calculadora = new Calculadora();
-//   // expect(calculadora).toBeDefined();
-//   expect(calculadora.resultado).toBeDefined();
-//   expect(typeof calculadora.resultado).toBe('number');
-// })
-
-// it('Atribui um novo valor à calculadora', () => {
-//   const calculadora = new Calculadora();
-//   expect(() => calculadora.resultado = "Texto").toThrow(TypeError);
-//   expect(() => calculadora.resultado = "Texto")
-//     .toThrow("O argumento deve ser um número válido");
-//   expect(calculadora.resultado).toBe(0);
-//   expect(() => calculadora.resultado = "5").not.toThrow(TypeError);
-//   expect(calculadora.resultado).toBe(5);
-// })
-
+  })
+});
 
